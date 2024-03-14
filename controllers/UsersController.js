2b6298ff2df0af6e98fa65764f7829482d983ea7
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2024-03-14 12:39:13 +0100

    Create a new user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,31 +1,30 @@
+import sha1 from 'sha1';
 import dbClient from '../utils/db';
-import bcrypt from 'bcrypt';
 
 class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
 
-    if (email === null) {
-      res.status(400).send('Missing email');
+    if (!email) {
+      res.status(400).json({ error: 'Missing email' });
     }
 
-    if (password === null) {
-      res.status(400).send('Missing password');
+    if (!password) {
+      res.status(400).json({ error: 'Missing password' });
     }
 
-    const collection = await dbClient.db.collection('users');
-    const user = await collection.findOne({ email });
-    if (user) {
-      res.status(400).send('Already exist');
+    const userExist = await dbClient.db.collection('users').findOne({ email });
+    if (userExist) {
+      res.status(400).json({ error: 'Already exist' });
     }
 
-    const hashed_passwd = await bcrypt.hash(password, 10);
-    user = {
-      email: email,
-      password: hashed_passwd
+    const hashedPasswd = sha1(password);
+    const user = {
+      email,
+      password: hashedPasswd,
     };
-    const result = await collection.insertOne(user);
-    res.status(201).json({email: email, id: result.insertedId})
+    const result = await dbClient.db.collection('users').insertOne(user);
+    res.status(201).json({ id: result.insertedId, email });
   }
 }
 
