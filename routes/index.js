9ea717c96ced8b6786dc5f9fe7fd90c7e2dd70ac
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2024-03-16 02:46:42 +0100

    updated

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
-const routes = express.Router();
+const router = express.Router();
 
-routes.get('/status', AppController.getStatus);
-routes.get('/stats', AppController.getStats);
-routes.post('/users', UsersController.postNew);
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
 
-export default routes;
+export default router;
